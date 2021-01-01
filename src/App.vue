07af1769ecb3a88bf9ae42cb0bc8d500edd38587
Tefoh <tefo.ha27@gmail.com>
2021-01-02 01:25:29 +0330

    58 - mini-project: create btn component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    
+    <BaseButton class="is-info">custom text</BaseButton>
+    <BaseButton color="primary" size="medium" :rounded="false" loading />
+    <BaseButton color="info" is-light size="small"  />
+    <BaseButton color="success" outlined @click="handleClick" :loading="isLoading" />
+    <BaseButton color="info" size="small" is-full />
+    <BaseButton color="danger" is-light />
+  </div>
+</template>
+
+<script>
+import BaseButton from './components/BaseButton.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  components: {
+    BaseButton
+  },
+  methods: {
+    handleClick() {
+      this.isLoading = true;
+    }
+  }
+}
+</script>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="button"
+    :class="{ 
+      [`is-${color}`]: color,
+      'is-light': isLight,
+      [`is-${size}`]: size,
+      'is-fullwidth': isFull,
+      'is-outlined': outlined,
+      'is-rounded': rounded,
+      'is-loading': loading
+    }"
+  >
+    <slot>our awesome button</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+
+  props: {
+    color: {
+      type: String,
+      required: false
+    },
+    isLight: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    isFull: {
+      type: Boolean,
+      default: false
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    },
+    rounded: {
+      type: Boolean,
+      default: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bulma/sass/utilities/_all.sass";
+
+@import 'bulma/sass/elements/button.sass';
+</style>
