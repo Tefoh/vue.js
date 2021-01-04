c6d37af80612884da9d1dbb689756955b25ae531
Tefoh <tefo.ha27@gmail.com>
2021-01-04 18:32:21 +0330

    62 - setup option

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  setup() {
+
+    const msg = 'Hello Vue 3.0 + Vite';
+    const count = 0;
+
+    console.log(msg)
+
+
+    return {
+      msg,
+      count
+    }
+  }
+
+  // data() {
+  //   return {
+  //     count: 0,
+  //     msg: 'Hello Vue 3.0 + Vite'
+  //   }
+  // }
+}
+</script>
