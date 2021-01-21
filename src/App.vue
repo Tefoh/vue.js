5e0e3d0f59c5dd6d4ff2e40f31f3f2ed2dc7ba71
Tefoh <tefo.ha27@gmail.com>
2021-01-21 15:53:10 +0330

    76 - jquery

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <p>Array: {{ arr }}</p>
-  <p>Changed Arr: {{ changedArr }}</p>
+  <button @click="handleHide">hide changed array</button>
+  <p id="changed-array">Changed Arr: {{ changedArr }}</p>
   <button @click="handleClick">clicked: {{ click }}</button>
 </template>
 
 <script>
-import { reactive, computed, ref } from 'vue';
-// import _ from 'lodash'
-import { partition, debounce } from 'lodash'
+import { reactive, computed, ref, onMounted } from 'vue';
+import _ from 'lodash'
+// import { partition, debounce } from 'lodash'
+import $ from 'jquery'
 
 export default {
   name: "App",
 
-  data: () => ({
+  /* data: () => ({
     click: 0,
     arr: [1, 2, 3, 4, 5, 6, 7, 8, 9]
   }),
@@ -27,10 +29,17 @@ export default {
   methods: {
     handleClick: debounce(function() {
       this.click++
-    }, 2000)
-  }
+    }, 2000),
+    handleHide() {
+      $('#changed-array').hide()
+    }
+  },
 
-  /* setup() {
+  mounted() {
+    $('#changed-array').css('color', 'white').css('background-color', 'red')
+  } */
+
+  setup() {
     const click = ref(0);
     const arr = reactive([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
@@ -42,13 +51,24 @@ export default {
       click.value++;
     }, 1000);
 
+    const handleHide = () => {
+      $('#changed-array').hide()
+    }
+
+    onMounted(() => {
+      $('#changed-array')
+        .css('color', 'white')
+        .css('background-color', 'red')
+    })
+
 
     return {
       click,
       arr,
       changedArr,
-      handleClick
+      handleClick,
+      handleHide
     }
-  } */
+  }
 }
 </script>
