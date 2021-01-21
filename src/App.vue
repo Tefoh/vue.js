90bf52ae642d5016503cb47b6471442d9400e2b8
Tefoh <tefo.ha27@gmail.com>
2021-01-21 13:13:44 +0330

    75 - lodash

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>Array: {{ arr }}</p>
+  <p>Changed Arr: {{ changedArr }}</p>
+  <button @click="handleClick">clicked: {{ click }}</button>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script>
+import { reactive, computed, ref } from 'vue';
+// import _ from 'lodash'
+import { partition, debounce } from 'lodash'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default {
+  name: "App",
+
+  data: () => ({
+    click: 0,
+    arr: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  }),
+
+  computed: {
+    changedArr() {
+      return partition(this.arr, n => n % 3)
+    }
+  },
+
+  methods: {
+    handleClick: debounce(function() {
+      this.click++
+    }, 2000)
+  }
+
+  /* setup() {
+    const click = ref(0);
+    const arr = reactive([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    const changedArr = computed(
+      () => _.partition(arr, n => n % 3)
+    );
+
+    const handleClick = _.throttle(() => {
+      click.value++;
+    }, 1000);
+
+
+    return {
+      click,
+      arr,
+      changedArr,
+      handleClick
+    }
+  } */
 }
-</style>
+</script>
