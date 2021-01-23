7cd79686e4a033d566bf1ac21d4974034be85225
Tefoh <tefo.ha27@gmail.com>
2021-01-23 22:30:27 +0330

    77 - vue-sweetalert

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <p>Array: {{ arr }}</p>
-  <button @click="handleHide">hide changed array</button>
-  <p id="changed-array">Changed Arr: {{ changedArr }}</p>
-  <button @click="handleClick">clicked: {{ click }}</button>
+  <button @click="handleAlert">show alert</button>
 </template>
 
 <script>
-import { reactive, computed, ref, onMounted } from 'vue';
-import _ from 'lodash'
-// import { partition, debounce } from 'lodash'
-import $ from 'jquery'
-
 export default {
   name: "App",
-
-  /* data: () => ({
-    click: 0,
-    arr: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  }),
-
-  computed: {
-    changedArr() {
-      return partition(this.arr, n => n % 3)
-    }
-  },
-
   methods: {
-    handleClick: debounce(function() {
-      this.click++
-    }, 2000),
-    handleHide() {
-      $('#changed-array').hide()
-    }
-  },
-
-  mounted() {
-    $('#changed-array').css('color', 'white').css('background-color', 'red')
-  } */
-
-  setup() {
-    const click = ref(0);
-    const arr = reactive([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-    const changedArr = computed(
-      () => _.partition(arr, n => n % 3)
-    );
-
-    const handleClick = _.throttle(() => {
-      click.value++;
-    }, 1000);
-
-    const handleHide = () => {
-      $('#changed-array').hide()
-    }
-
-    onMounted(() => {
-      $('#changed-array')
-        .css('color', 'white')
-        .css('background-color', 'red')
-    })
-
-
-    return {
-      click,
-      arr,
-      changedArr,
-      handleClick,
-      handleHide
+    handleAlert() {
+      this.$swal({
+        title: 'سلام دوستان عزیز',
+        icon: 'warning',
+        confirmButtonText: 'اوکی',
+        showCancelButton: true,
+        showDenyButton: true,
+        cancelButtonText: 'کنسل',
+        denyButtonText: 'ذخیره نکن'
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.$swal('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          this.$swal('Changes are not saved', '', 'info')
+        }
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSweetAlert from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueSweetAlert)
+  .mount('#app')
