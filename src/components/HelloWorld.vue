8d55ba7d67fa9a50de1e0a0bb8cbd07f43fdc6b7
Tefoh <tefo.ha27@gmail.com>
2021-01-31 15:08:18 +0330

    80 - fetch api get text info

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button @click="handleClick">get text from file</button>
+  <p>
+    {{ text }}
+  </p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  props: ['msg'],
+
+  setup() {
+    const text = ref('خالی');
+
+    const handleClick = () => {
+
+      fetch('/text.txt')
+        .then(response => response.text())
+        .then(message => text.value = message);
+    };
+
+    return {
+      text,
+      handleClick
+    };
+  }
+
+  // data: () => ({
+  //   text: 'empty'
+  // }),
+  // methods: {
+  //   handleClick() {
+  //     fetch('/text.txt')
+  //       .then(response => {
+  //         return response.text();
+  //       })
+  //       .then(message => {
+  //         this.text = message
+  //       })
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
