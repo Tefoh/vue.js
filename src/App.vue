6e2a76363b623aa50c16b5d1cf06bb677692afea
Tefoh <tefo.ha27@gmal.com>
2021-03-14 18:04:54 +0330

    124 - setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header class="header">
+        <div class="header__shape">
+            <div class="header__shape-inner"></div>
+        </div>
+        <div class="container">
+            <div class="navbar">
+                <div class="navbar__row">
+                    <div class="header__logo"><router-link :to="{ to: 'Home' }" class="header__logo-img"></router-link></div>
+                    <div class="navbar__items">
+                        <ul class="navbar__ul">
+                            <li class="navbar__item"><router-link :to="{ to: 'Home' }" class="navbar__link navbar__link--is-active">صفحه اصلی</router-link></li>
+                            <li class="navbar__item navbar__item--has-sub"><a href="" class="navbar__link">فروشگاه</a>
+                                <ul class="navbar__subset">
+                                    <li class="navbar__item"><a href="" class="navbar__link">زیر منو 1</a></li>
+                                    <li class="navbar__item"><a href="" class="navbar__link">زیر منو 2</a></li>
+                                    <li class="navbar__item navbar__item--has-sub"><a href="" class="navbar__link">زیر منو 3</a>
+                                        <ul class="navbar__subset">
+                                            <li class="navbar__item"><a href="" class="navbar__link">زیر منو 1</a></li>
+                                            <li class="navbar__item"><a href="" class="navbar__link">زیر منو 2</a></li>
+                                            <li class="navbar__item navbar__item--has-sub"><a href="" class="navbar__link">زیر منو 3</a>
+                                                <ul class="navbar__subset">
+                                                    <li class="navbar__item"><a href="" class="navbar__link">زیر منو 1</a></li>
+                                                    <li class="navbar__item"><a href="" class="navbar__link">زیر منو 2</a></li>
+                                                    <li class="navbar__item"><a href="" class="navbar__link">زیر منو 3</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="navbar__item"><a href="" class="navbar__link">درباره ما</a></li>
+                            <li class="navbar__item"><a href="" class="navbar__link">تماس باما</a></li>
+                        </ul>
+                    </div>
+                    <div class="navbar__action">
+                        <div class="header__basket">
+                            <span class="header__basket-icon"></span>
+                            <span class="header__basket-count">0</span>
+                            <div class="header__dropdown">
+                                <div class="header__dropdown-content header__dropdown-content--overflow">
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                    <p>1</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header__account">
+                            <span class="header__account-icon"></span>
+                            <div class="header__dropdown header__dropdown--w200">
+                                <div class="header__dropdown-content">account</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/font.css'
+import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
