d41cf7ba3e64234ac419d79456a5155aa9df83dc
Tefoh <tefo.ha27@gmal.com>
2021-04-06 17:16:18 +0430

    145 - refactor tabs

---
+++ src/components/Tabs/CommentsTab.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="tab__section tab__section--comments">
+      <div class="comments" id="comments">
+          <div class="comments__send">
+              <div class="comments__title">
+                  <h3 class="comments__h3">دیدگاه خود را بنویسد</h3>
+                  <span class="comments__count">دیدگاه ها ( 150 )</span>
+              </div>
+              <textarea class="txt txt--textarea" placeholder="بنویسد"></textarea>
+              <button class="btn btn--brand btn--boxshadow">ارسال نظر</button>
+              <button class="btn btn--cancel btn--boxshadow-cancel">انصراف</button>
+          </div>
+      </div>
+
+      <div class="comments__list">
+          <div id="comment__1">
+              <div class="comments__box">
+                  <div class="comments__inner">
+                      <div class="comments__header">
+                          <div class="comments__row">
+                              <div class="comments__info">
+                                  <div class="comments__avatar"><img src="img/profile.jpg" class="comments__img" alt=""></div>
+                                  <div class="comments__details">
+                                      <h5 class="comments__author"><span class="comments__author-name">محمد نیکو</span></h5>
+                                      <span class="comments__date">555 روز پیش</span>
+                                  </div>
+                              </div>
+                              <a href="#comments" class="btn btn--brand btn--reply btn--boxshadow">پاسخ</a>
+                          </div>
+                      </div>
+                      <div class="comments__body">
+                          لورم ایپسوم متـــــــــــــــــــــــــــــــــــــــــــــــــــن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div id="comment__2">
+              <div class="comments__box">
+                  <div class="comments__inner">
+                      <div class="comments__header">
+                          <div class="comments__row">
+                              <div class="comments__info">
+                                  <div class="comments__avatar"><img src="img/profile.jpg" class="comments__img" alt=""></div>
+                                  <div class="comments__details">
+                                      <h5 class="comments__author"><span class="comments__author-name">محمد نیکو</span></h5>
+                                      <span class="comments__date">555 روز پیش</span>
+                                  </div>
+                              </div>
+                              <a href="#comments" class="btn btn--brand btn--reply btn--boxshadow">پاسخ</a>
+                          </div>
+                      </div>
+                      <div class="comments__body">
+                          لورم ایپسوم متـــــــــــــــــــــــــــــــــــــــــــــــــــن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+                      </div>
+                  </div>
+                  <div class="comments__subset">
+                      <div id="comment__3">
+                          <div class="comments__box">
+                              <div class="comments__inner">
+                                  <div class="comments__header">
+                                      <div class="comments__row">
+                                          <div class="comments__info">
+                                              <div class="comments__avatar"><img src="img/profile.jpg" class="comments__img" alt=""></div>
+                                              <div class="comments__details">
+                                                  <h5 class="comments__author"><span class="comments__author-name">محمد نیکو</span></h5>
+                                                  <span class="comments__date">555 روز پیش</span>
+                                              </div>
+                                          </div>
+                                          <a href="#comments" class="btn btn--brand btn--reply btn--boxshadow">پاسخ</a>
+                                      </div>
+                                  </div>
+                                  <div class="comments__body">
+                                      لورم ایپسوم متـــــــــــــــــــــــــــــــــــــــــــــــــــن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+
+                                  </div>
+                              </div>
+                              <div class="comments__subset">
+                                  <div id="comment__4">
+                                      <div class="comments__box">
+                                          <div class="comments__inner">
+                                              <div class="comments__header">
+                                                  <div class="comments__row">
+                                                      <div class="comments__info">
+                                                          <div class="comments__avatar"><img src="img/profile.jpg" class="comments__img" alt=""></div>
+                                                          <div class="comments__details">
+                                                              <h5 class="comments__author"><span class="comments__author-name">محمد نیکو</span></h5>
+                                                              <span class="comments__date">555 روز پیش</span>
+                                                          </div>
+                                                      </div>
+                                                      <a href="#comments" class="btn btn--brand btn--reply btn--boxshadow">پاسخ</a>
+                                                  </div>
+                                              </div>
+                                              <div class="comments__body">
+                                                  لورم ایپسوم متـــــــــــــــــــــــــــــــــــــــــــــــــــن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Tabs/FeaturesTab.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="tab__section tab__section--featrues">
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+      <div class="tab__section-field">
+          <div class="col-3">
+              <h6 class="tab__section-name">ویژیگی 1</h6>
+          </div>
+          <div class="col-9">
+              <div class="tab__section-value">متن کوتاه قرار بگیرد</div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
