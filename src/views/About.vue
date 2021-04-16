4e04e1ecb4de6b1cbb46ffffb3590e73c14c0ed4
Tefoh <tefo.ha27@gmal.com>
2021-04-16 23:55:56 +0430

    159 - composition-api

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,22 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+
+export default {
+  name: "About",
+
+  setup() {
+    const store = useStore();
+
+    store.dispatch('products/getProducts', '')
+
+    return {
+      products: computed(() => store.state.products.products)
+    }
+  }
+}
+</script>
