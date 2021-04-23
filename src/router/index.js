b29020c351b7e6e807d116b123ff094989428368
Tefoh <tefo.ha27@gmal.com>
2021-04-23 18:53:46 +0430

    152 - fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -45,7 +45,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   base: process.env.BASE_URL,
   routes
 })
