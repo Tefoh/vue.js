f7504956e7c8e6fe86a072727c554967d7156618
Tefoh <tefo.ha27@gmal.com>
2021-04-25 22:57:14 +0430

    164 - named slots

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot name="title" />
+
+
+    <slot name="default" />
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,27 @@
     <BaseBtn type="submit">
      ذخیره
     </BaseBtn>
+    <BaseCard>
+      <template #title>
+        <h1>my card title</h1>
+      </template>
+      
+      <p>test body</p>
+
+
+    </BaseCard>
   </div>
 </template>
 
 <script>
 import BaseBtn from '@/components/BaseBtn.vue'
+import BaseCard from '@/components/BaseCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    BaseBtn
+    BaseBtn,
+    BaseCard
   }
 }
 </script>
