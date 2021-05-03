0d20cf55d1292d5d225e4cd2eb29a5f114f7197a
Tefoh <tefo.ha27@gmal.com>
2021-05-04 01:57:49 +0430

    182 - grid system

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,44 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="3">
+        <v-card color="blue">
+          <v-card-text class="d-flex white--text">
+            <v-icon class="blue--text text--lighten-5">mdi-account-group</v-icon>
+            <v-spacer></v-spacer>
+            <span>کاربران</span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card color="orange">
+          <v-card-text class="d-flex white--text">
+            <v-icon class="orange--text text--lighten-5">mdi-post</v-icon>
+            <v-spacer></v-spacer>
+            <span>پست ها</span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card color="green">
+          <v-card-text class="d-flex white--text">
+            <span>سفارشات</span>
+            <v-spacer></v-spacer>
+            <v-icon class="green--text text--lighten-5">mdi-cart</v-icon>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card color="indigo">
+          <v-card-text class="d-flex white--text">
+            <span>محصولات</span>
+            <v-spacer></v-spacer>
+            <v-icon class="indigo--text text--lighten-5">mdi-cart-plus</v-icon>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -7,9 +46,5 @@
 
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
