6ffca77c468b89cb609701b56413bd7005ff262c
Tefoh <tefo.ha27@gmal.com>
2021-05-06 00:38:00 +0430

    185 - data-table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,12 +40,86 @@
     </v-row>
 
     <v-divider class="mt-4"></v-divider>
+
+    <p class="text-h4 font-weight-medium my-5">لیست کاربران</p>
+
+    <v-data-table :headers="titles" :items="users"></v-data-table>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
+
+  data: () => ({
+    titles: [
+      { text: 'اسم', value: 'name' },
+      { text: 'ایمیل', value: 'email' },
+      { text: 'اسم کاربری', value: 'username' },
+    ],
+    users: [
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+      },
+      {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net",
+      },
+      {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org",
+      },
+      {
+        id: 5,
+        name: "Chelsey Dietrich",
+        username: "Kamren",
+        email: "Lucio_Hettinger@annie.ca",
+      },
+      {
+        id: 6,
+        name: "Mrs. Dennis Schulist",
+        username: "Leopoldo_Corkery",
+        email: "Karley_Dach@jasper.info",
+      },
+      {
+        id: 7,
+        name: "Kurtis Weissnat",
+        username: "Elwyn.Skiles",
+        email: "Telly.Hoeger@billy.biz",
+      },
+      {
+        id: 8,
+        name: "Nicholas Runolfsdottir V",
+        username: "Maxime_Nienow",
+        email: "Sherwood@rosamond.me",
+      },
+      {
+        id: 9,
+        name: "Glenna Reichert",
+        username: "Delphine",
+        email: "Chaim_McDermott@dana.io",
+      },
+      {
+        id: 10,
+        name: "Clementina DuBuque",
+        username: "Moriah.Stanton",
+        email: "Rey.Padberg@karina.biz",
+}
+]
+  }),
 
   created() {
     // this.$vuetify.theme.dark = true;
