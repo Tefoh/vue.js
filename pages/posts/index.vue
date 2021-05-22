21c16bda0918b7559578f74f2667e3a61180ec70
Tefoh <tefo.ha27@gmal.com>
2021-05-23 00:05:28 +0430

    203 - fetch

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+
     <!-- <NuxtLink :to="`/posts/${2}`">پست شماره 2</NuxtLink> -->
     <NuxtLink :to="{ name: 'posts-id', params: { id: 4 } }">پست شماره 4</NuxtLink>
     <NuxtLink to="/users">لیست کاربران</NuxtLink>
@@ -8,9 +9,15 @@
     <Logo />
     <BaseInput />
     <LazyBaseButton />
-    <button class="button is-info"
+    <!-- <button class="button is-info"
       @click="$toast('شما یک پیغام دریافت کردید')"
-    >الرت</button>
+    >الرت</button> -->
+    <button class="button is-info"
+      @click="refresh"
+    >دریافت دوباره اطلاعات</button>
+    <p v-if="$fetchState.pending">در حال دریافت اطلاعات مقالات</p>
+    <p v-if="$fetchState.error">هنگام دریافت اطلاعات به مشکل خوردیم</p>
+    <p v-for="(post, index) in posts" :key="post.id">{{ `${index + 1}: ${post.title}` }}</p>
   </div>
 </template>
 
@@ -24,8 +31,14 @@ export default {
   data: () => ({
     posts: []
   }),
-  async created() {
-    this.posts = await this.$axios.$get('/users')
+  async fetch() {
+    this.posts = await this.$axios.$get('/posts')
+  },
+  methods: {
+    refresh() {
+      this.posts = [];
+      this.$fetch();
+    }
   }
 }
 </script>
