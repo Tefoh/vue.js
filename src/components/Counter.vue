6f3062574126a4ea6081650707d0349226411291
Tefoh <tefo.ha27@gmail.com>
2021-06-13 00:27:23 +0430

    214 - setup vue ts with vue-cli

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="inc">counter: {{ number }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    number: 7
+  }),
+  methods: {
+    inc() {
+      this.number++
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
