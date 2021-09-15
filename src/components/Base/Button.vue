73e3af82cf0374a016e29b70b4cf54099a86c318
tefoh <tefo.ha27@gmail.com>
2021-09-15 21:26:19 +0430

    236 - v-bind

--- src/components/Base/Button.vue
+++ src/components/Base/Button.vue
@@ -12,16 +12,29 @@ export default {
   // third party plugins
   // title
 
-  inheritAttrs: false
+  inheritAttrs: false,
+  // data: () => ({
+  //   colors: {
+  //     bg: 'green',
+  //     color: 'white'
+  //   }
+  // })
 }
 </script>
 
 <script setup>
+import { reactive } from 'vue'
+
 const props = defineProps({
   counter: Number
 })
 
 const emits = defineEmits(['inc'])
+
+const colors = reactive({
+  bg: 'blue',
+  color: 'white'
+})
 
 const increment = () => {
   console.log(props.counter);
@@ -29,3 +42,16 @@ const increment = () => {
   emits('inc', props.counter, 'salam')
 }
 </script>
+
+<style>
+button {
+  /* when using usual script
+
+   background-color: v-bind('colors.bg');
+  color: v-bind('colors.color'); */
+
+  /* when using <script setup> */
+  background-color: v-bind('colors.bg');
+  color: v-bind('colors.color');
+}
+</style>
